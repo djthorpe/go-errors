d71a6749e294d198f04b0900ac34303d793b2136
David Thorpe <djt@mutablelogic.com>
2021-09-03 08:35:18 +0200

    Added errors file

---
+++ errors.go
@@ -0,0 +1,59 @@
+package errors
+
+import "fmt"
+
+///////////////////////////////////////////////////////////////////////////////
+// TYPES
+
+type Error uint
+
+///////////////////////////////////////////////////////////////////////////////
+// GLOBALS
+
+const (
+	ErrSuccess Error = iota
+	ErrBadParameter
+	ErrDuplicateEntry
+	ErrUnexpectedResponse
+	ErrNotFound
+	ErrNotModified
+	ErrInternalAppError
+	ErrNotImplemented
+	ErrOutOfOrder
+)
+
+///////////////////////////////////////////////////////////////////////////////
+// STRINGIFY
+
+func (e Error) Error() string {
+	switch e {
+	case ErrSuccess:
+		return "ErrSuccess"
+	case ErrBadParameter:
+		return "ErrBadParameter"
+	case ErrDuplicateEntry:
+		return "ErrDuplicateEntry"
+	case ErrUnexpectedResponse:
+		return "ErrUnexpectedResponse"
+	case ErrNotFound:
+		return "ErrNotFound"
+	case ErrNotModified:
+		return "ErrNotModified"
+	case ErrInternalAppError:
+		return "ErrInternalAppError"
+	case ErrNotImplemented:
+		return "ErrNotImplemented"
+	case ErrOutOfOrder:
+		return "ErrOutOfOrder"
+	default:
+		return "[?? Invalid Error value]"
+	}
+}
+
+func (e Error) With(args ...interface{}) error {
+	return fmt.Errorf("%w: %s", e, fmt.Sprint(args...))
+}
+
+func (e Error) Withf(format string, args ...interface{}) error {
+	return fmt.Errorf("%w: %s", e, fmt.Sprintf(format, args...))
+}
